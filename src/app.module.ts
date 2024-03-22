a2a545115c4c2219fdb842777adb7e97701976f0
EthicalRalph <rakinola90@gmail.com>
2024-03-22 23:35:06 +0100

    chore: database(mongodb) orm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSource } from './database/datasource';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({}),
-      dataSourceFactory: async () => dataSource.initialize(),
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('POSTGRES_DATABASE_URL'),
+        synchronize: true,
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_DATABASE_URL'),
+      }),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
     }),
   ],
   controllers: [AppController],
